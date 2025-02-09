7b8941ed3fe67056125a6173a74524395fe3b1f2
N0T4 <wurdalaka337@gmail.com>
2025-02-09 15:10:19 +0000

    sign-up

--- app/(auth)/sing-up.jsx
+++ app/(auth)/sing-up.jsx
@@ -4,6 +4,7 @@ import {images} from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link } from 'expo-router'
+import { createUser } from '../../lib/appwrite'
 
 const SingUp = () => {
   const [form, setform] = useState({
@@ -15,7 +16,7 @@ const SingUp = () => {
   const [isSubmitting, setIsSubmitting] = useState(false)
 
   const submit =() =>{
-
+    createUser();
   }
   return (
     <SafeAreaView className='bg-primary h-full'>
